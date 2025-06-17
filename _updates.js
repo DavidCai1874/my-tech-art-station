c29f2b6a8ddbd49d29fdb07948719ccf531e9dad
DavidCai1874 <DavidCai1874@users.noreply.github.com>
2025-06-17 19:55:02 +0000

    deploy: 3d375804d81994640f3f32f758cb9a0be9ae717c

---
+++ _updates.js
@@ -0,0 +1,17 @@
+const updates = [
+  { date: "2025-06-16", text: "Forgot to write the weekly log yesterday (Sunday), so I'm making it up today, lol. P.S. This update is one day after the log was written, mainly because I'm cleaning the git history of my code repo, so I thought of committing my website too." },
+  { date: "2025-06-09", text: "Began fixing the weekly logs page, changed the color" },
+  { date: "2025-06-08", text: "Tried to implement the searching in the navigation bar and updated the WIP page." },
+  { date: "2025-06-07", text: "Changed the color palette, cleaned up index.css, fixed the bug where pages go to the left side only by defining CSS for the root html, added search bar code, deployed to see if it works, and worked a bit on the WIP page." },
+  { date: "2025-06-02", text: "Finished the framework and added content to the WIP and Weekly Log pages. Committed to GitHub." },
+  { date: "2025-06-01", text: "Started my very first weekly log." },
+  { date: "2025-05-25", text: "Finished planning the navigation bar, renamed the page folders with index, and kept the order the same as the navigation bar." },
+  { date: "2025-05-24", text: "Planned how to sustain the site in the long term and organized the file structure." },
+  { date: "2025-05-22", text: "Changed the navigation bar UI a bit and almost finished the home page." },
+  { date: "2025-05-19", text: "Added a navigation bar and created all the needed pages (with a few words, lol)." },
+  { date: "2025-05-16", text: "Site was deployed to GitHub." },
+  { date: "2025-05-11", text: "Site was built locally with Vite." },
+];
+
+// This file contains the updates for the website.
+export default updates;
