02253f9547a7f7eb55beb2faa7da83fd6d6f2427
DavidC1874 <davidcai1874@gmail.com>
2025-09-11 16:59:33 -0700

    don't fail

--- dist/_updates.js
+++ dist/_updates.js
@@ -1,4 +1,5 @@
 const updates = [
+  { date: "2025-09-11", text: "Did the Maya section for ts, and wrote my first doc!" },
   { date: "2025-09-10", text: "Added a new section for Pipeline Trouble Shooting. Adjusted the layout of issues. Made two codes to generate addon and ts issues to certain .js files" },
   { date: "2025-09-09", text: "Used slicing to fixed the problem that title shows twice, adjusted the # and tags' positions. Removed descriptions for addons and ts" },
   { date: "2025-09-07", text: "Fixed Github Page 404 problem.And changed the way to show cover image of the weekly log page from local image to link." },
