d7a9a9617b3904287246cf716042c7ed05c5982f
David Cai <ycai53318@ecuad.ca>
2025-10-04 12:29:30 -0700

    usd doc

--- src/pages/06_TroubleShooting/Pipeline/TS_pipeline.js
+++ src/pages/06_TroubleShooting/Pipeline/TS_pipeline.js
@@ -1,5 +1,6 @@
 import ts_p_0001 from './mds/ts-p-0001.md?raw';
 import ts_p_0002 from './mds/ts-p-0002.md?raw';
+import ts_p_0003 from './mds/ts-p-0003.md?raw';
 
 // auto-generated file for Pipeline
 const TS_Pipeline = [
@@ -14,6 +15,12 @@ const TS_Pipeline = [
     id: "ts-p-0002",
     date: "2025-09-26",
     md: ts_p_0002
+  },
+  {
+    title: "How To Import USD File From Maya To Unreal Engine 5",
+    id: "ts-p-0003",
+    date: "2025-10-03",
+    md: ts_p_0003
   }
 ];
 export default TS_Pipeline;
---
+++ src/pages/09_TodoMDs/_usually_things.md
@@ -0,0 +1,3 @@
+**Windows -> Settings/Preferences -> Plug-in Manager**
+
+pop-up window
