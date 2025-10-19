7c437c5cc84dc8ddff5ffef4a9b19a8cfa9d8976
David Cai <ycai53318@ecuad.ca>
2025-10-19 15:23:33 -0700

    Oct 19 weekly update - game jam done!

--- src/pages/08_WeeklyLog/Era/2025_09Fall/2025_09fall.js
+++ src/pages/08_WeeklyLog/Era/2025_09Fall/2025_09fall.js
@@ -3,8 +3,8 @@ import md2025_09_14 from './mds/2025-09-14.md?raw';
 import md2025_09_21 from './mds/2025-09-21.md?raw';
 import md2025_09_28 from './mds/2025-09-28.md?raw';
 import md2025_10_05 from './mds/2025-10-05.md?raw';
-// import md2025_10_12 from './mds/2025-10-12.md?raw';
-// import md2025_10_19 from './mds/2025-10-19.md?raw';
+import md2025_10_12 from './mds/2025-10-12.md?raw';
+import md2025_10_19 from './mds/2025-10-19.md?raw';
 // import md2025_10_26 from './mds/2025-10-26.md?raw';
 // import md2025_11_02 from './mds/2025-11-02.md?raw';
 // import md2025_11_09 from './mds/2025-11-09.md?raw';
@@ -27,8 +27,8 @@ const weeks = [
   // { md: md2025_11_09, date: "2025-11-09", label: "Week 10 (Nov 9)" },
   // { md: md2025_11_02, date: "2025-11-02", label: "Week 9 (Nov 2)" },
   // { md: md2025_10_26, date: "2025-10-26", label: "Week 8 (Oct 26)" },
-  // { md: md2025_10_19, date: "2025-10-19", label: "Week 7 (Oct 19)" },
-  // { md: md2025_10_12, date: "2025-10-12", label: "Week 6 (Oct 12)" },
+  { md: md2025_10_19, date: "2025-10-19", label: "Week 7 (Oct 19)" },
+  { md: md2025_10_12, date: "2025-10-12", label: "Week 6 (Oct 12)" },
   { md: md2025_10_05, date: "2025-10-05", label: "Week 5 (Oct 5)" },
   { md: md2025_09_28, date: "2025-09-28", label: "Week 4 (Sep 28)" },
   { md: md2025_09_21, date: "2025-09-21", label: "Week 3 (Sep 21)" },
