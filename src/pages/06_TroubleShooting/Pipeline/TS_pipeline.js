88bcd842d02eeacd3f09f1fbeda27d65a8d5c1d8
David Cai <ycai53318@ecuad.ca>
2025-11-05 17:17:11 -0800

    Perforce Doc on single user optiming

--- src/pages/06_TroubleShooting/Pipeline/TS_pipeline.js
+++ src/pages/06_TroubleShooting/Pipeline/TS_pipeline.js
@@ -2,6 +2,7 @@ import ts_p_0001 from './mds/ts-p-0001.md?raw';
 import ts_p_0002 from './mds/ts-p-0002.md?raw';
 import ts_p_0003 from './mds/ts-p-0003.md?raw';
 import ts_p_0004 from './mds/ts-p-0004.md?raw';
+import ts_p_0005 from './mds/ts-p-0005.md?raw';
 
 // auto-generated file for Pipeline
 const TS_Pipeline = [
@@ -28,6 +29,12 @@ const TS_Pipeline = [
     id: "ts-p-0004",
     date: "2025-11-01",
     md: ts_p_0004
+  },
+  {
+    title: "How to Configure Settings In P4V To Get the Best Single-User Experience",
+    id: "ts-p-0005",
+    date: "2025-11-05",
+    md: ts_p_0005
   }
 ];
 export default TS_Pipeline;
